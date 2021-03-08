8199e4bd455a205399de0e772d5fb05544ff79f7
Wutthiwat Sornso <wutthiwat.sornso@quantiumfund.com>
2021-03-09 04:21:21 +0700

    Initial tabs

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomeScreen from './Screens/HomeScreen';
+import ConvertScreen from './Screens/ConvertScreen';
+import SettingsScreen from './Screens/SettingsScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -25,8 +12,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Convert' component={ConvertScreen} />
+        <Tab.Screen name='Settings' component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
